98177d052abcfa8fc3da32208dff16787fabbc4f
Kateryna2289Taranenko <katuha2289@ukr.net>
2023-03-12 20:27:56 +0200

    add 1-2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const galleryEl = document.querySelector('.gallery');
+function createGalleryItems(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" 
+      href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+const cardsMarkup = createGalleryItems(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup);
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
